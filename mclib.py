3c563952564f4e6d6c0e4c737184ad89bcd999aa
PCF-Testing <m.shamraeva@gmail.com>
2010-07-21 04:23:31 -0700

    Signed-off-by: PCF-Testing <m.shamraeva@gmail.com>

--- mclib.py
+++ mclib.py
@@ -1,6 +1,11 @@
 # Module MCLIB.PY
 # includes:
 #   * function remove_html_tags(data) - strips Python string of HTML tags
+#   * function remove_punctuation(data) - removes punctuation signs and other
+#              meta characters from a string
+#   * function remove_digits(data) - removes digits from a string
+#   * function split_by_punctuation_char(data) - truncates the string argument
+#              by the first punctuation character
 #   * subroutine AppendErrorMessage(self,sel,msg) - inserts <msg> error message
 #                into verificationErrors list and into output log
 
@@ -9,6 +14,23 @@ import re, testvars
 def remove_html_tags(data):
     p = re.compile(r'<.*?>')
     return p.sub('', data)
+
+def remove_punctuation(data):
+    p = re.compile(r'(\W+)')
+    return p.sub(' ',data)    # A punctuation sign is replaced with a whitespace
+
+def remove_digits(data):
+    p = re.compile(r'(\d)')
+    return p.sub('',data)    # Digits are eliminated without replacement
+
+def split_by_punctuation_char(data):
+    # Split the argument string by:
+    # period, comma, semicolon, colon, opening bracket, closing bracket,
+    # opening square bracket, closing square bracket, hyphen, underscore,
+    # slash, backslash
+    # DO NOT split by whitespace
+    p = re.split(r'[.,;:\(\)\[\]\-\_/\\]+', data)
+    return p[0]
 
 
 def AppendErrorMessage(self,sel,msg):
