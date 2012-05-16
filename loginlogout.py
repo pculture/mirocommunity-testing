210425d7d5a9c9340cbb606336726959e163dc1c
Janet <jed@pculture>
2012-05-16 10:06:56 +0200

    login button css

--- loginlogout.py
+++ loginlogout.py
@@ -45,7 +45,7 @@ def LogInBasic(self, sel, username, password):
     sel.click("id_password")
     sel.type("id_password", password)
     time.sleep(1)
-    sel.click("css=form div.controls button")
+    sel.click("css=div.left input.button")
     time.sleep(7)
 #    sel.wait_for_page_to_load(testvars.MCTestVariables["TimeOut"])
     mclib.wait_for_element_present(self, sel, testvars.MCTestVariables["LogoutFootlink"])
